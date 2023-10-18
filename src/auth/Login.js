f9e97d7867f34d2f34a9a3025c719c3101d44d74
prius2055 <prius2055@gmail.com>
2023-10-18 05:32:06 +0100

    Finish login and registration pages

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -44,37 +44,48 @@ function Login() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            name="username"
-            placeholder="Username"
-            required
-            value={user.username}
-            onChange={handleChange}
-          />
-        </div>
+    <div className="flex relative h-full w-full">
+      <div className="bg-gradient-to-tr from-yellow-800 to-yellow-800 h-24 min-h-full w-1/2">
+        <img
+          src="https://images.unsplash.com/photo-1470615619213-fdd3985a40e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2054&q=80"
+          className="w-full h-full object-cover mix-blend-overlay"
+        />
+      </div>
 
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-            value={user.password}
-            onChange={handleChange}
-          />
-        </div>
+      <form
+        onSubmit={handleSubmit}
+        className="mx-20 my-auto py-60 border-2 border-yellow-500 flex flex-col w-1/2 rounded-md"
+      >
+     
+        <input
+          className="w-3/4 rounded-md border-0 py-1.5 pl-5 pr-20 mx-auto my-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 focus:outline-0 sm:text-sm sm:leading-6"
+          type="text"
+          name="username"
+          placeholder="Username"
+          required
+          value={user.username}
+          onChange={handleChange}
+        />
 
-        <button type="submit" className="btn btn-primary btn-sm">
+        <input
+          className="w-3/4 rounded-md border-0 py-1.5 pl-5 pr-20 mx-auto my-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-yellow-500 focus:outline-0 sm:text-sm sm:leading-6"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={user.password}
+          onChange={handleChange}
+        />
+    
+
+        <button
+          type="submit"
+          className="w-2/4 rounded-md bg-lime-600 px-3 py-2 mx-auto text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500"
+        >
           Login
         </button>
-        <p>
-          Don't have an account? <Link to="/registration">Register</Link>
+        <p className="mx-auto w-2/4 text-center my-4">
+          Don't have an account? <Link to="/registration" className='underline text-yellow-900'>Register</Link>
         </p>
       </form>
     </div>
--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
-// import Image from '../img/yellow_building-removebg.png'
-
-import './SplashPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const SplashPage = () => {
   return (
-    <div className="bg-gradient-to-tr from-yellow-500 to-yellow-500 h-24 min-h-full relative">
+    <div className="bg-gradient-to-tr from-yellow-800 to-yellow-800 h-24 min-h-full relative">
       <img
         src="https://images.unsplash.com/photo-1470615619213-fdd3985a40e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2054&q=80"
         className="w-full h-full object-cover mix-blend-overlay absolute"
       />
-      <div className="h-screen flex items-center justify-center flex-col">
-        <h1 className="text-6xl font-bold ">Welcome to propInspect</h1>
-        <p className="text-6l font-bold my-8">
+      <div className="h-screen flex items-center justify-center flex-col absolute w-full">
+        <h1 className="text-6xl font-bold text-white">
+          Welcome to propInspect
+        </h1>
+        <p className="text-3xl my-8 text-white">
           Your one-stop showroom for real estate properties
         </p>
-        <div className="text-white font-bold bg-lime-900 py-2 px-12 rounded-full cursor-pointer">
+        <div className="text-white text-xl font-bold bg-lime-500 py-2 px-4 rounded-full cursor-pointer flex items-center justify-between w-32">
           <Link to="/login">Sign in</Link>
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            style={{ color: '#ffffff' }}
+          />
         </div>
       </div>
     </div>

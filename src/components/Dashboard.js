fbd37c7c16be4e4c879458b3e41048f919f1b8a5
prius2055 <prius2055@gmail.com>
2023-10-30 07:07:37 +0100

    add styling to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,8 +10,94 @@ const Dashboard = () => {
   //   }
 
   return (
-    <div>
+    <div className="flex h-screen">
       <Navigation />
+      <div className="flex flex-col items-center py-8 w-3/4">
+        <header className="my-4 font-bold">LATEST PROPERTIES</header>
+        <p>Please select a property to book for an inspection</p>
+        <div className="flex justify-between mt-8 items-center">
+          <div className="text-center mx-4">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Omega house, Gwarimpa"
+              className="w-80 h-80 rounded-full m-auto"
+            />
+            <div className="flex justify-center my-6 text-center font-bold">
+              <span className="mr-4">Omega house</span>
+              <span>OM001</span>
+            </div>
+            <p>
+              Beautiful living Condo surronded by nature. This Condo is suitable
+              for anyoune who likes nature and enjoys meditation. Located in the
+              heart of Gwarimpa, Seventh avenue
+            </p>
+          </div>
+          <div className="text-center mx-4">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Omega house, Gwarimpa"
+              className="w-80 h-80 rounded-full m-auto mb-8"
+            />
+            <div className="flex justify-center my-6 text-center font-bold">
+              <span className="mr-4">Omega house</span>
+              <span>OM001</span>
+            </div>
+            <p>
+              Beautiful living Condo surronded by nature. This Condo is suitable
+              for anyoune who likes nature and enjoys meditation. Located in the
+              heart of Gwarimpa, Seventh avenue
+            </p>
+          </div>
+          <div className="text-center mx-4">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Omega house, Gwarimpa"
+              className="w-80 h-80 rounded-full m-auto mb-8"
+            />
+            <div className="flex justify-center my-6 text-center font-bold">
+              <span className="mr-4">Omega house</span>
+              <span>OM001</span>
+            </div>
+            <p>
+              Beautiful living Condo surronded by nature. This Condo is suitable
+              for anyoune who likes nature and enjoys meditation. Located in the
+              heart of Gwarimpa, Seventh avenue
+            </p>
+          </div>
+          {/* <div className="text-center mx-4">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Omega house, Gwarimpa"
+              className="w-80 h-80 rounded-full m-auto mb-8"
+            />
+            <div className="flex justify-center my-6 text-center font-bold">
+              <span className="mr-4">Omega house</span>
+              <span>OM001</span>
+            </div>
+            <p>
+              Beautiful living Condo surronded by nature. This Condo is suitable
+              for anyoune who likes nature and enjoys meditation. Located in the
+              heart of Gwarimpa, Seventh avenue
+            </p>
+          </div> */}
+          {/* <div className="text-center mx-4">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Omega house, Gwarimpa"
+              className="w-80 h-80 rounded-full m-auto mb-8"
+            />
+            <div className="flex justify-center my-6 text-center font-bold">
+              <span className="mr-4">Omega house</span>
+              <span>OM001</span>
+            </div>
+            <p>
+              Beautiful living Condo surronded by nature. This Condo is suitable
+              for anyoune who likes nature and enjoys meditation. Located in the
+              heart of Gwarimpa, Seventh avenue
+            </p>
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faVimeoV } from '@fortawesome/free-brands-svg-icons';
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons';
 
 const Navigation = () => {
   return (
-    <div className="flex flex-col justify-between h-screen w-1/4 py-4 px-4">
-      <h1>propInspect</h1>
-      <ul>
-        <li>Properties</li>
-        <li>My appointments</li>
-        <li>Book appointment</li>
-        <li>Add property</li>
-        <li>Remove property</li>
-      </ul>
-      <button>Logout</button>
-      <div>
-        <div>
+    <div className="flex flex-col justify-between h-screen w-1/4 py-4 pl-4 border-2">
+      <div className="flex flex-col font-bold">
+        <h1 className="p-4">propInspect</h1>
+
+        <ul className="mt-20 flex flex-col justify-between h-80 uppercase cursor-pointer ">
+          <li className="p-4 hover:bg-lime-500">Properties</li>
+          <li className="p-4 hover:bg-lime-500">My appointments</li>
+          <li className="p-4 hover:bg-lime-500">Book appointment</li>
+          <li className="p-4 hover:bg-lime-500">Add property</li>
+          <li className="p-4 hover:bg-lime-500">Remove property</li>
+          <li className="p-4 hover:bg-lime-500">Logout out</li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-start ">
+        <div className="flex justify-between mb-4 mr-20">
           <FontAwesomeIcon icon={faXTwitter} style={{ color: '#000000' }} />
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faGooglePlusG} />
+          <FontAwesomeIcon icon={faVimeoV} />
+          <FontAwesomeIcon icon={faPinterestP} />
         </div>
-        <button>copyright</button>
+        <footer className="text-sm">&copy; 2023 PropInspect</footer>
       </div>
     </div>
   );

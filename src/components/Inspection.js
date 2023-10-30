91b5f61652fd814ff128ae7d5b7f71993be960ba
prius2055 <prius2055@gmail.com>
2023-10-30 19:02:02 +0100

    JUpdate styling on inspection page

--- src/components/Inspection.js
+++ src/components/Inspection.js
@@ -8,24 +8,24 @@ import Navigation from './Navigation';
 
 const Inspection = () => {
   return (
-    <div className="flex h-screen bg-lime-500">
+    <div className="flex h-screen">
       <Navigation />
-      <div className="flex flex-col justify-evenly items-center py-8 w-5/6 m-auto">
+      <div className="flex flex-col items-center py-8 w-5/6 bg-lime-500">
         <h1 className="uppercase border-solid border-b border-gray-200 pb-2 font-bold w-1/2 text-center text-3xl text-white tracking-widest">
           Book an inspection
         </h1>
 
-        <div className="w-1/2">
-          <div className="flex justify-end mt-6 font-bold text-white">
+        <div className="w-1/2 my-4">
+          <div className="flex justify-end font-bold text-white">
             <span className="mr-4">Omega house</span>
             <span>OM001</span>
           </div>
           <img
             src="https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?auto=format&fit=crop&q=80&w=2028&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Omega house, Gwarimpa"
-            className="w-full h-1/2 rounded-lg m-auto"
+            className="w-full h-3/4 rounded-lg m-auto"
           />
-          <p className="mt-6 mb-10 text-white w-full text-center">
+          <p className="mt-6 mb-2 text-white w-full text-center">
             Beautiful living Condo surronded by nature. This Condo is suitable
             for anyoune who likes nature and enjoys meditation. Located in the
             heart of Gwarimpa, Seventh avenue

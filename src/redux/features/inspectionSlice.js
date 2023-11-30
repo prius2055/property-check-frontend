ad278dab66b761c8e06bf6c4e1d299326a3d6439
prius2055 <prius2055@gmail.com>
2023-11-30 21:58:55 +0100

    update url

--- src/redux/features/inspectionSlice.js
+++ src/redux/features/inspectionSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'https://propcheck-api.onrender.com/inspections';
+const url = 'https://propcheck-api.onrender.com/api/v1/inspections';
 
 export const getInspections = createAsyncThunk('get/inspection', async () => {
   const authToken = localStorage.getItem('token');
@@ -41,7 +41,7 @@ export const getUserInspections = createAsyncThunk(
   async (id) => {
     const authToken = localStorage.getItem('token');
     const response = await axios.get(
-      `https://propcheck-api.onrender.com/user_inspection/${id}`,
+      `https://propcheck-api.onrender.com/api/v1/user_inspection/${id}`,
       {
         headers: {
           'content-type': 'application/json',
@@ -60,7 +60,7 @@ export const deleteInspection = createAsyncThunk(
   async (id) => {
     const authToken = localStorage.getItem('token');
     const response = await axios.delete(
-      `https://propcheck-api.onrender.com/inspections/${id}`,
+      `https://propcheck-api.onrender.com/api/v1/inspections/${id}`,
       {
         headers: {
           'content-type': 'application/json',
